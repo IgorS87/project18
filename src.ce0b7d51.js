de93af59157ec61669169d36182de6d5681df539
RomanLeibiuk <92354812+RomanLeibiuk@users.noreply.github.com>
2022-01-14 18:04:47 +0000

    Deploying to gh-pages from @ RomanLeibiuk/project18@e1639caef561e6a98d15b95e3735b851fe4735ac 🚀

--- src.ce0b7d51.js
+++ src.ce0b7d51.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project18/src.31a564f5.js.map
+//# sourceMappingURL=/project18/src.ce0b7d51.js.map
